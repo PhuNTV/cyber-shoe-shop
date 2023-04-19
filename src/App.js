6cb7bd0f829d6ccc654507e54bd26b677367d7e1
Raymond Nguyen <phuntv.jp@gmail.com>
2023-04-19 20:14:34 +0700

    upcode

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import ShoeStore from './Components/ShoeStore';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ShoeStore />
+    </div>
+  );
+}
+
+export default App;
